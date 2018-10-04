f125b926545c8677dc36b742ca9f102e50e6f855
Jeremy Wilson <jeremybwilson@gmail.com>
2018-10-04 01:27:38 -0700

    Added basic login and reg pages, basic views and url routes to support

--- apps/devs/urls.py
+++ apps/devs/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
+    url(r'^register$', views.register, name="register"),
+    url(r'^create$', views.create, name="create"),
+    url(r'^login/$', views.login, name="login"),
+    url(r'^loginprocess$', views.loginprocess, name="loginprocess"),
     url(r'^$', views.index, name="index"),
 ]
--- apps/devs/views.py
+++ apps/devs/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+import re, bcrypt
 
 # Create your views here.
+def index(request):
+    if 'dev_id' not in request.session:
+        return redirect('devs:register')
+
+    if 'logged_in_status' not in request.session:
+        return redirect('devs:register')
+    
+def register(request):
+    return render(request, 'devs/register.html')
+
+def create(request):
+    # if request.method == 'POST':
+    pass
+
+def login(request):
+    return render(request, 'devs/login.html')
+
+def loginprocess(request):
+    # if request.method == 'POST':
+    pass
